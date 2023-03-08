bcf6d8d43bf80eacccec268b1aa1a1f08272350b
TimoWolle <timosch3@gmx.de>
2023-03-08 13:28:17 +0100

    Aufgabe 1 // Datentypenzuordnung

---
+++ src/aufgabe1/Aufgabe1.java
@@ -0,0 +1,51 @@
+package aufgabe1;
+
+public class Aufgabe1 {
+    // Welchen der folgenden Datentypen int long float double haben folgende Ausdrücke und welchen Wert nehmen sie nach dem Durchlaufen der Anwendung an?
+
+    // a) 5 / Int
+    int a = 5;
+    // = 5
+
+    // b) 5.0 / Double
+    double b = 5.0;
+    // = 5.0
+
+    // c) 4L / String
+    long c = 4;
+    // = 4
+
+    // d) 3 + 9 / Int
+    int d = 3 + 9;
+    // = 12
+
+    // e) 2 + 6.0
+    double e = 2 + 6.0;
+    // = 8
+
+    // f) 5 + 3.0f
+    double f = 5 + 3.0;
+    // = 8
+
+    // g) 3d + 1.0f
+    double g = 3 + 1.0;
+    // = 4
+
+    // h) 6L * 3
+    long h= 6 * 3;
+    // = 18
+
+    // i) 7 / 2
+    double i = 7 / 2;
+    // = 3.5
+
+    // j) 9 / 2.0
+    double j = 9 / 2.0;
+    // = 4.5
+
+    // k) int i = 2; i--;
+    int x = 2;
+    int k = x--;
+    // = 1
+
+}
