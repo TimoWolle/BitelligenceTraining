7aec95be5ab61676ae76d9b455ca1008367e8414
TimoWolle <timosch3@gmx.de>
2023-03-09 16:26:58 +0100

    SelectionSort // Dateieerstellung ohne Inhalt

---
+++ src/sorting/bubble_sort/Selection_Sort.java
@@ -0,0 +1,20 @@
+package sorting.bubble_sort;
+
+public class Selection_Sort {
+
+    /**
+     * This method sorts an array of integers using the selection sort algorithm.
+     * Selection sort is a simple sorting algorithm that finds the smallest element
+     * in an unsorted array and swaps it with the first element.
+     * This process is repeated until the entire array is sorted.
+     *
+     * @param numbers The array of integers to be sorted
+     * @return The sorted array of integers
+     */
+
+    public int[] sort(int[] numbers) {
+        // ...
+        return numbers;
+    }
+
+}
